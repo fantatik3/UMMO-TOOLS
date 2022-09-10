1618f2e8609da59b37b02c071721a026a6cadbd3
Sema <fantatikdj@gmail.com>
2022-09-10 08:01:13 +0200

    File structure changes

---
+++ Assets/CustomEditorWindow/Editor/CREATION_TOOLS/TOOL_ENUMS.cs
@@ -0,0 +1,23 @@
+namespace CREATION_TOOLS
+{
+    public enum GAME_CURRENCY {
+        SILVER = 0,
+        CONTRIBUTION_POINTS = 1,
+        CONTRIBUTION_MEDAL = 2,
+        EXPLORERS_MARK = 3,
+        SHATTERED_FRAGMENTS = 4,
+        HOLY_WOOD = 5
+    }
+
+    public enum QUEST_SYSTEM_CONFIG {
+        MAX_OBJETIVE_NUM = 5,
+        MAX_LOOT_SLOT_NUM = 10,
+        MAX_CURRENCY_REWARDS = 4
+    }
+
+    public enum TOOL_CONFIG
+    {
+        HEADER_PADDING = 15,
+        ELEMENT_PADDING = 5
+    } 
+}
---
+++ Assets/CustomEditorWindow/Editor/CREATION_TOOLS/TOOL_STRUCTS.cs
@@ -0,0 +1,15 @@
+namespace CREATION_TOOLS
+{
+    public struct sOBJETIVES
+    {
+        public int type;
+        public int amount;
+    }
+
+    public struct sLOOT
+    {
+        public int itemID;
+        public int amount;
+        public int chance;
+    }
+}
