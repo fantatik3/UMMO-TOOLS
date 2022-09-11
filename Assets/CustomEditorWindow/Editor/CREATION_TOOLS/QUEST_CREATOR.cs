b7f14964416a9a32a279d87dd870873da61e606b
Sema <fantatikdj@gmail.com>
2022-09-11 07:15:27 +0200

    Removed unneeded variables and updated the quest name set and read

--- Assets/CustomEditorWindow/Editor/CREATION_TOOLS/QUEST_CREATOR.cs
+++ Assets/CustomEditorWindow/Editor/CREATION_TOOLS/QUEST_CREATOR.cs
@@ -1,6 +1,5 @@
 using UnityEditor;
 using UnityEngine;
-using System.Collections;
 
 namespace CREATION_TOOLS
 {
@@ -15,27 +14,25 @@ namespace CREATION_TOOLS
         Vector2 scrollPos = new Vector2();
         #endregion
 
-        #region MEMBERS
-
-        int _Index = 0;
-        string _Name = "New Quest";
-        QUEST_TYPE _Type = new QUEST_TYPE();
-        int _Level = 0;
-        QUEST_NPC_TYPE _NPCType = new QUEST_NPC_TYPE();
-        float _AvatarExperience = 0;
-        float _PetExperience = 0;
-
-        sCURRENCY[] _CurrencyReward = new sCURRENCY[(int)QUEST_SYSTEM_CONFIG.MAX_CURRENCY_REWARDS];
-        sOBJETIVES[] _ObjetiveData = new sOBJETIVES[(int)QUEST_SYSTEM_CONFIG.MAX_OBJETIVE_NUM];
-        sLOOT[] _LootData = new sLOOT[(int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM];
-
-        #endregion
-
+        static QUEST_DATA _QuestData = new QUEST_DATA();
+        static int _QuestCount = 0;
 
         [MenuItem("MMORPG Tools/Quest/New...")]
-        public static void ShowWindow()
+
+
+        public static void ShowWindow() { 
+
+   
+         
+        _ThisWindow = GetWindow(typeof(QUEST_CREATOR));
+        }
+        void Awake()
         {
-            _ThisWindow = GetWindow(typeof(QUEST_CREATOR));
+            _QuestData.name = new byte[(int)QUEST_SYSTEM_CONFIG.MAX_QUEST_NAME_LENGTH];
+            _QuestData.name = System.Text.Encoding.ASCII.GetBytes("New Quest");
+            _QuestData.currencyReward = new sCURRENCY[(int)QUEST_SYSTEM_CONFIG.MAX_CURRENCY_REWARDS];
+            _QuestData.objetiveData = new sOBJETIVES[(int)QUEST_SYSTEM_CONFIG.MAX_OBJETIVE_NUM];
+            _QuestData.lootData = new sLOOT[(int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM];
         }
 
         private void OnGUI()
@@ -54,27 +51,29 @@ namespace CREATION_TOOLS
             GUILayout.Label("Fill the quest data.", EditorStyles.boldLabel);
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
 
-            _Index = EditorGUILayout.IntField("Index:", _Index);
-            GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
-            _Name = EditorGUILayout.TextField("Quest Name:", _Name);
+            _QuestData.index = EditorGUILayout.IntField("Index:", _QuestData.index);
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
-            _Type = (QUEST_TYPE)EditorGUILayout.EnumPopup("Type:", _Type);
+            _QuestData.name = System.Text.Encoding.ASCII.GetBytes(EditorGUILayout.TextField("Quest Name:", System.Text.Encoding.ASCII.GetString(_QuestData.name)));
+
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
-            _Level = EditorGUILayout.IntField("Level:", _Level);
+            _QuestData.type = (QUEST_TYPE)EditorGUILayout.EnumPopup("Type:", _QuestData.type);
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
-            _NPCType = (QUEST_NPC_TYPE)EditorGUILayout.EnumPopup("NPCType:", _NPCType);
+            _QuestData.level = (int)EditorGUILayout.Slider("Level:", _QuestData.level, 0.0f, (int)GAME_CONFIG.MAX_AVATAR_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_GOD_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_REBIRTH_LEVEL);
+            GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
+
+            _QuestData.NPCType = (QUEST_NPC_TYPE)EditorGUILayout.EnumPopup("NPCType:", _QuestData.NPCType);
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
             GUILayout.Label("EXP REWARDS:", EditorStyles.boldLabel);
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
 
-            _AvatarExperience = EditorGUILayout.Slider("Experience:", _AvatarExperience, 0.0f, 100.0f);
+            _QuestData.avatarExperience = EditorGUILayout.Slider("Experience:", _QuestData.avatarExperience, 0.0f, 100.0f);
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
-            _PetExperience = EditorGUILayout.Slider("Pet Exp:", _PetExperience, 0.0f, 100.0f);
+            _QuestData.petExperience = EditorGUILayout.Slider("Pet Exp:", _QuestData.petExperience, 0.0f, 100.0f);
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
@@ -85,9 +84,9 @@ namespace CREATION_TOOLS
             {
                 GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
                 GUILayout.Label("Currency " + (i + 1) + ":", EditorStyles.boldLabel);
-                _CurrencyReward[i].currency = (GAME_CURRENCY)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _CurrencyReward[i].currency);
-                _CurrencyReward[i].amount = EditorGUILayout.IntField("Amount:" + (i + 1), _CurrencyReward[i].amount);
-    
+                _QuestData.currencyReward[i].currency = (GAME_CURRENCY)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.currencyReward[i].currency);
+                _QuestData.currencyReward[i].amount = EditorGUILayout.IntField("Amount" + (i + 1) + ":", _QuestData.currencyReward[i].amount);
+
             }
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
@@ -98,8 +97,8 @@ namespace CREATION_TOOLS
             {
                 GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
                 GUILayout.Label("Objetive " + (i + 1) + ":", EditorStyles.boldLabel);
-                _ObjetiveData[i].objetive = (QUEST_OBJETIVES)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _ObjetiveData[i].objetive);
-                _ObjetiveData[i].amount = EditorGUILayout.IntField("Req. Amount:", _ObjetiveData[i].amount);
+                _QuestData.objetiveData[i].objetive = (QUEST_OBJETIVES)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.objetiveData[i].objetive);
+                _QuestData.objetiveData[i].amount = EditorGUILayout.IntField("Req. Amount:", _QuestData.objetiveData[i].amount);
 
             }
 
@@ -111,9 +110,9 @@ namespace CREATION_TOOLS
             {
                 GUILayout.Label("Item Slot_" + (i + 1) + ":", EditorStyles.boldLabel);
                 GUILayout.Space(1);
-                _LootData[i].itemID = EditorGUILayout.IntField("Item ID:", _LootData[i].itemID);
-                _LootData[i].amount = EditorGUILayout.IntField("Amount:", _LootData[i].amount);
-                _LootData[i].chance = EditorGUILayout.Slider("Chance:", _LootData[i].chance, 0.0f, 100.0f);
+                _QuestData.lootData[i].itemID = EditorGUILayout.IntField("Item ID:", _QuestData.lootData[i].itemID);
+                _QuestData.lootData[i].amount = EditorGUILayout.IntField("Amount:", _QuestData.lootData[i].amount);
+                _QuestData.lootData[i].chance = EditorGUILayout.Slider("Chance:", _QuestData.lootData[i].chance, 0.0f, 100.0f);
 
                 GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
             }
@@ -122,32 +121,37 @@ namespace CREATION_TOOLS
             GUILayout.FlexibleSpace();
 
             if (GUILayout.Button("SAVE")) {
-                SaveFile();
+                SaveToFile();
             }
 
         }
 
         // Start is called before the first frame update
-        void SaveFile() {
+        void SaveToFile() {
 
-            if (_Index < 0) {
+            if (_QuestData.index < 0) {
                 Debug.LogError("[ERROR] Index cant be less than 0.");
                 return;
             }
-            if (_Name.Equals(""))
+            if (_QuestData.name.Equals(""))
             {
                 Debug.LogError("[ERROR] Quest name cant be empty.");
                 return;
             }
+            if (_QuestData.name.Length > (int)QUEST_SYSTEM_CONFIG.MAX_QUEST_NAME_LENGTH)
+            {
+                Debug.LogError("[ERROR] Quest name is too long.");
+                return;
+            }
 
-            if (_Level < 0 || _Level > (int)GAME_CONFIG.MAX_AVATAR_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_GOD_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_REBIRTH_LEVEL)
+            if (_QuestData.level < 0 || _QuestData.level > (int)GAME_CONFIG.MAX_AVATAR_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_GOD_LEVEL + (int)GAME_CONFIG.MAX_AVATAR_REBIRTH_LEVEL)
             {
                 Debug.LogError("[ERROR] Quest name cant be empty.");
                 return;
             }
 
             for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_CURRENCY_REWARDS; i++) {
-                if (_CurrencyReward[i].amount < 0)
+                if (_QuestData.currencyReward[i].amount < 0)
                 {
                     Debug.LogError("[ERROR] Currency Reward Amount cant be less than 0.");
                     return;
@@ -155,7 +159,7 @@ namespace CREATION_TOOLS
             }
             for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_OBJETIVE_NUM; i++)
             {
-                if (_ObjetiveData[i].amount < 0)
+                if (_QuestData.objetiveData[i].amount < 0)
                 {
                     Debug.LogError("[ERROR] Objetive Amount cant be less than 0.");
                     return;
@@ -164,22 +168,22 @@ namespace CREATION_TOOLS
 
             for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM; i++)
             {
-                if (_LootData[i].itemID < 0 || _LootData[i].itemID > 99999)
+                if (_QuestData.lootData[i].itemID < 0 || _QuestData.lootData[i].itemID > 99999)
                 {
                     Debug.LogError("[ERROR] Wrong Item ID in Loot Slot: " + (i + 1));
                     return;
                 }
-                if (_LootData[i].amount < 0 || _LootData[i].amount > (int)TOOL_CONFIG.MAX_REWARD_ITEM_AMOUNT)
+                if (_QuestData.lootData[i].amount < 0 || _QuestData.lootData[i].amount > (int)TOOL_CONFIG.MAX_REWARD_ITEM_AMOUNT)
                 {
                     Debug.LogError("[ERROR] Wrong Item Amount in Loot Slot: " + (i + 1));
                     return;
                 }
             }
-
-
-
+            _QuestCount++;
+            ToolSaving.SaveQuest(_QuestCount, _QuestData);
             Debug.LogError("ALL WENT OK!");
         }
 
+      
     }
 }
