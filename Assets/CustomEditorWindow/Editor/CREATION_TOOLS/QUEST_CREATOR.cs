7ae7e237d340f3683f93da45671babcd3bb33e1b
Sema <fantatikdj@gmail.com>
2022-09-11 09:49:36 +0200

    Main script rework to use EditorGUISplitView

--- Assets/CustomEditorWindow/Editor/CREATION_TOOLS/QUEST_CREATOR.cs
+++ Assets/CustomEditorWindow/Editor/CREATION_TOOLS/QUEST_CREATOR.cs
@@ -5,27 +5,47 @@ namespace CREATION_TOOLS
 {
     public class QUEST_CREATOR : EditorWindow
     {
-        static EditorWindow _ThisWindow;
-
         #region GUILAYOUT_MEMBERS
 
         GUIStyle _GUIStyle;
-        GUIContent _GUIContent;
+       /* GUIContent _GUIContent;*/
         Vector2 scrollPos = new Vector2();
         #endregion
 
         static QUEST_DATA _QuestData = new QUEST_DATA();
         static int _QuestCount = 0;
 
+        EditorGUISplitView horizontalSplitView = new EditorGUISplitView(EditorGUISplitView.Direction.Horizontal);
+        EditorGUISplitView verticalSplitView = new EditorGUISplitView(EditorGUISplitView.Direction.Vertical);
+
+        bool showCurrency = false;
+        bool showObjetives = false;
+        bool showLootTable = false;
+
         [MenuItem("MMORPG Tools/Quest/New...")]
-
-
-        public static void ShowWindow() { 
-
-   
-         
-        _ThisWindow = GetWindow(typeof(QUEST_CREATOR));
+        static void Init()
+        {
+            QUEST_CREATOR window = (QUEST_CREATOR)GetWindow(typeof(QUEST_CREATOR));
+            window.Show();
+            window.title = "QUEST CREATION TOOL";
         }
+
+        public void OnGUI()
+        {
+            horizontalSplitView.BeginSplitView();
+            DrawQuestViewer();
+            horizontalSplitView.Split();
+            DrawQuestEditor();
+            horizontalSplitView.EndSplitView();
+            Repaint();
+        }
+
+
+        void DrawQuestViewer()
+        {
+            GUILayout.Label("QUEST LIST VIEWER GOES HERE", EditorStyles.boldLabel);
+        }
+
         void Awake()
         {
             _QuestData.name = new byte[(int)QUEST_SYSTEM_CONFIG.MAX_QUEST_NAME_LENGTH];
@@ -35,11 +55,7 @@ namespace CREATION_TOOLS
             _QuestData.lootData = new sLOOT[(int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM];
         }
 
-        private void OnGUI()
-        {
-            //Set Window name
-            _ThisWindow.title = "QUEST CREATION TOOL";
-     
+        void DrawQuestEditor() {
             scrollPos = GUILayout.BeginScrollView(scrollPos);
 
             //Set Window Style
@@ -50,9 +66,6 @@ namespace CREATION_TOOLS
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
             GUILayout.Label("Fill the quest data.", EditorStyles.boldLabel);
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
-
-            _QuestData.index = EditorGUILayout.IntField("Index:", _QuestData.index);
-            GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
             _QuestData.name = System.Text.Encoding.ASCII.GetBytes(EditorGUILayout.TextField("Quest Name:", System.Text.Encoding.ASCII.GetString(_QuestData.name)));
 
@@ -68,62 +81,67 @@ namespace CREATION_TOOLS
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
             GUILayout.Label("EXP REWARDS:", EditorStyles.boldLabel);
-            GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
+            GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
             _QuestData.avatarExperience = EditorGUILayout.Slider("Experience:", _QuestData.avatarExperience, 0.0f, 100.0f);
             GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
 
             _QuestData.petExperience = EditorGUILayout.Slider("Pet Exp:", _QuestData.petExperience, 0.0f, 100.0f);
-            GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
-
-            GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
-            GUILayout.Label("CURRENCY REWARDS:", EditorStyles.boldLabel);
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
 
-            for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_CURRENCY_REWARDS; i++)
+            showCurrency = EditorGUILayout.Foldout(showCurrency, "CURRENCY REWARDS");
+            if (showCurrency)
             {
-                GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
-                GUILayout.Label("Currency " + (i + 1) + ":", EditorStyles.boldLabel);
-                _QuestData.currencyReward[i].currency = (GAME_CURRENCY)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.currencyReward[i].currency);
-                _QuestData.currencyReward[i].amount = EditorGUILayout.IntField("Amount" + (i + 1) + ":", _QuestData.currencyReward[i].amount);
+                for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_CURRENCY_REWARDS; i++)
+                {
+                    GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
+                    GUILayout.Label("Currency " + (i + 1) + ":", EditorStyles.boldLabel);
+                    _QuestData.currencyReward[i].currency = (GAME_CURRENCY)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.currencyReward[i].currency);
+                    _QuestData.currencyReward[i].amount = EditorGUILayout.IntField("Amount" + (i + 1) + ":", _QuestData.currencyReward[i].amount);
 
+                }
             }
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
-            GUILayout.Label("OBJETIVES:", EditorStyles.boldLabel);
-            GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
 
-            for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_OBJETIVE_NUM; i++)
+            showObjetives = EditorGUILayout.Foldout(showObjetives, "OBJETIVES");
+            if (showObjetives)
             {
-                GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
-                GUILayout.Label("Objetive " + (i + 1) + ":", EditorStyles.boldLabel);
-                _QuestData.objetiveData[i].objetive = (QUEST_OBJETIVES)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.objetiveData[i].objetive);
-                _QuestData.objetiveData[i].amount = EditorGUILayout.IntField("Req. Amount:", _QuestData.objetiveData[i].amount);
+                for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_OBJETIVE_NUM; i++)
+                {
+                    GUILayout.Space((int)TOOL_CONFIG.ELEMENT_PADDING);
+                    GUILayout.Label("Objetive " + (i + 1) + ":", EditorStyles.boldLabel);
+                    _QuestData.objetiveData[i].objetive = (QUEST_OBJETIVES)EditorGUILayout.EnumPopup("Type " + (i + 1) + ":", _QuestData.objetiveData[i].objetive);
+                    _QuestData.objetiveData[i].amount = EditorGUILayout.IntField("Req. Amount:", _QuestData.objetiveData[i].amount);
 
+                }
             }
 
             GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
-            GUILayout.Label("LOOT TABLE:", EditorStyles.boldLabel);
-            GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
 
-            for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM; i++)
+            showLootTable = EditorGUILayout.Foldout(showLootTable, "LOOT TABLE");
+            if (showLootTable)
             {
-                GUILayout.Label("Item Slot_" + (i + 1) + ":", EditorStyles.boldLabel);
-                GUILayout.Space(1);
-                _QuestData.lootData[i].itemID = EditorGUILayout.IntField("Item ID:", _QuestData.lootData[i].itemID);
-                _QuestData.lootData[i].amount = EditorGUILayout.IntField("Amount:", _QuestData.lootData[i].amount);
-                _QuestData.lootData[i].chance = EditorGUILayout.Slider("Chance:", _QuestData.lootData[i].chance, 0.0f, 100.0f);
+                for (int i = 0; i < (int)QUEST_SYSTEM_CONFIG.MAX_LOOT_SLOT_NUM; i++)
+                {
+                    GUILayout.Label("Item Slot_" + (i + 1) + ":", EditorStyles.boldLabel);
+                    GUILayout.Space(1);
+                    _QuestData.lootData[i].itemID = EditorGUILayout.IntField("Item ID:", _QuestData.lootData[i].itemID);
+                    _QuestData.lootData[i].amount = EditorGUILayout.IntField("Amount:", _QuestData.lootData[i].amount);
+                    _QuestData.lootData[i].chance = EditorGUILayout.Slider("Chance:", _QuestData.lootData[i].chance, 0.0f, 100.0f);
 
-                GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
+                    GUILayout.Space((int)TOOL_CONFIG.HEADER_PADDING);
+                }
             }
 
-            GUILayout.EndScrollView();
+            _QuestData.index = _QuestCount;
             GUILayout.FlexibleSpace();
 
-            if (GUILayout.Button("SAVE")) {
+            if (GUILayout.Button("Insert Quest"))
+            {
                 SaveToFile();
             }
-
+            GUILayout.EndScrollView();
         }
 
         // Start is called before the first frame update
